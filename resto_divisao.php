b69540df73bad447c378b818063e73c140177558
nubiapaivaa <nubia.p.silva7@aluno.senai.br>
2025-08-14 10:44:28 -0300

    Atividades PHP

---
+++ resto_divisao.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = 10;
+$numero2 = 3;
+
+$resto = $numero1 % $numero2;
+$div = $numero1 / $numero2;
+
+echo "A divisão". $numero1. "/". $numero2. "=". $div. "<br>" ."e o resto da divisão é: ". $resto;
+//echo $resto; //Saída: 1(pois 10 dividido por 3 tem resto de 1)
+?> 
---
+++ variavel.php
@@ -0,0 +1,6 @@
+<?php
+    $nome= "Maria";
+    $idade = 25;
+    echo $nome . " tem " . $idade . " anos.";
+?>
+
