b424ff9fbfaa484c779724122d7677139537b176
nubiapaivaa <nubia.p.silva7@aluno.senai.br>
2025-08-14 11:48:19 -0300

    laço e if

---
+++ laco_if.php
@@ -0,0 +1,20 @@
+<?php
+    $resposta = "não";
+    $resposta = "Sua mae deixou voce ir ao MCdonald";
+
+    if($resposta == "sim"){
+    echo "O rolê está ON!";
+    }else{
+        echo "Hoje não terá rolê";
+    }
+
+    echo "<br>";
+    $numero1 = 100;
+    $numero2 = 200;
+
+    if($numero1 >= $numero2){
+        echo "O $numero1 é maior ou igual que o $numero2";  
+    }else{
+        echo "O $numero2 é maior ou igual que o $numero1";
+    }
+?>
